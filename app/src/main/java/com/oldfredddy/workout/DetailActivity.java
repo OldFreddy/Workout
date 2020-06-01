bb8d5345790043e7e263088bb107807247fb5111
Freddy <goblinblin@mail.ru>
2020-06-01 20:22:07 +0300

    Initial commit

---
+++ app/src/main/java/com/oldfredddy/workout/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.oldfredddy.workout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_frag);
+        frag.setWorkout(1);
+    }
+}
